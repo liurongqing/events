37f7d9cf3279761f09c1682b5db3c90262318e0c
liurongqing <liurongqing@xsyxsc.com>
2021-09-09 10:01:29 +0800

    1

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { IEventEmitter } from "./IEventEmitter";
 import { EventEmitter, Emit, On } from "./index";
 
 const emitter: IEventEmitter = new EventEmitter()
-
+// test
 On(emitter, 'update', (...rest)=>{
   console.log('update1...', rest)
 })
